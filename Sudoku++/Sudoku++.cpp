71223f3a4baf02ef55efc17bd65477caf5184fb9
Sergey <isergey207207@gmail.com>
2023-03-31 14:38:33 +0300

    Добавлено выделение клетки Теперь для пользовательских цифр будет другой шрифт

--- Sudoku++/Sudoku++.cpp
+++ Sudoku++/Sudoku++.cpp
@@ -9,6 +9,16 @@ using namespace std;
 #define FIELD_BORDER 20
 #define DOWN_SIZE 100
 
+struct tablePos {
+    tablePos(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+    int x;
+    int y;
+};
+
 void drawTable(RenderWindow* window)
 {
     for (int i = 0; i <= 9; i++)
@@ -25,10 +35,9 @@ void drawTable(RenderWindow* window)
     }
 }
 
-void drawNums(RenderWindow* window, Font* font, int table[9][9])
+void drawNums(RenderWindow* window, Font* regular_font, Font* bold_font, int var[9][9], int table[9][9])
 {
     Text text;
-    text.setFont(*font);
     text.setFillColor(Color::Black);
     text.setCharacterSize(40);
     for (int i = 0; i < 9; i++)
@@ -37,6 +46,7 @@ void drawNums(RenderWindow* window, Font* font, int table[9][9])
         {
             if (table[i][j])
             {
+                text.setFont((var[i][j] == 0) ? *regular_font : *bold_font);
                 text.setString(to_string(table[i][j]));
                 text.setPosition(Vector2f(j * CELL_SIZE + FIELD_BORDER * 1.6, i * CELL_SIZE + FIELD_BORDER));
                 (*window).draw(text);
@@ -45,28 +55,48 @@ void drawNums(RenderWindow* window, Font* font, int table[9][9])
     }
 }
 
+tablePos processClick(Vector2f pos)
+{
+    pos.x -= FIELD_BORDER;
+    pos.y -= FIELD_BORDER;
+    return tablePos(pos.x / CELL_SIZE, pos.y / CELL_SIZE);
+}
+
 int main()
 {
-
-    int var_1[9][9] = {
-          { 0, 0, 0, 0, 0, 8, 0, 0, 1 },
-          { 0, 0, 8, 0, 4, 0, 0, 0, 0 },
-          { 0, 0, 3, 0, 9, 1, 0, 4, 2 },
-          { 0, 3, 1, 0, 0, 0, 0, 0, 0 },
-          { 6, 0, 0, 7, 5, 0, 0, 0, 0 },
-          { 0, 2, 0, 3, 0, 6, 0, 9, 5 },
-          { 0, 6, 2, 8, 0, 0, 0, 0, 4 },
-          { 0, 0, 5, 9, 0, 4, 0, 0, 7 },
-          { 0, 0, 4, 0, 0, 0, 2, 0, 9 }
-    };
-
-    RenderWindow window(VideoMode(9 * CELL_SIZE + 2 * FIELD_BORDER, 9 * CELL_SIZE + 2 * FIELD_BORDER + DOWN_SIZE), 
+    RenderWindow window(VideoMode(9 * CELL_SIZE + 2.3 * FIELD_BORDER, 9 * CELL_SIZE + 2.3 * FIELD_BORDER + DOWN_SIZE), 
         L"Sudoku++", Style::Close);
 
     window.setVerticalSyncEnabled(true);
-    Font font;
-    if (!font.loadFromFile("Disket-Mono-Bold.ttf"))
+    Font regular_font, bold_font;
+    if (!regular_font.loadFromFile("Disket-Mono-Regular.ttf") || !bold_font.loadFromFile("Disket-Mono-Bold.ttf"))
         return 0;
+
+    int var_1[9][9] = {
+      { 0, 0, 0, 0, 0, 8, 0, 0, 1 },
+      { 0, 0, 8, 0, 4, 0, 0, 0, 0 },
+      { 0, 0, 3, 0, 9, 1, 0, 4, 2 },
+      { 0, 3, 1, 0, 0, 0, 0, 0, 0 },
+      { 6, 0, 0, 7, 5, 0, 0, 0, 0 },
+      { 0, 2, 0, 3, 0, 6, 0, 9, 5 },
+      { 0, 6, 2, 8, 0, 0, 0, 0, 4 },
+      { 0, 0, 5, 9, 0, 4, 0, 0, 7 },
+      { 0, 0, 4, 0, 0, 0, 2, 0, 9 }
+    };
+
+    int table[9][9] = {
+      { 0, 0, 0, 0, 0, 8, 0, 0, 1 },
+      { 0, 0, 8, 0, 4, 0, 0, 0, 0 },
+      { 0, 0, 3, 0, 9, 1, 8, 4, 2 },
+      { 0, 3, 1, 0, 0, 0, 0, 0, 0 },
+      { 6, 0, 0, 7, 5, 0, 0, 0, 0 },
+      { 0, 2, 0, 3, 0, 6, 0, 9, 5 },
+      { 0, 6, 2, 8, 0, 0, 0, 0, 4 },
+      { 0, 0, 5, 9, 0, 4, 0, 0, 7 },
+      { 0, 0, 4, 0, 0, 0, 2, 0, 9 }
+    };
+
+    tablePos choosenPos = tablePos(-1, -1);
 
     while (window.isOpen())
     {
@@ -75,12 +105,23 @@ int main()
         {
             if (event.type == Event::Closed)
                 window.close();
+            if (event.type == Event::MouseButtonReleased)
+            {
+                choosenPos = processClick(Vector2f(event.mouseButton.x, event.mouseButton.y));
+            }
         }
         
         window.clear(Color(188, 152, 126, 255));
+        if (choosenPos.x != -1)
+        {
+            RectangleShape rect(Vector2f(CELL_SIZE, CELL_SIZE));
+            rect.setPosition(Vector2f(choosenPos.x * CELL_SIZE + FIELD_BORDER, choosenPos.y * CELL_SIZE + FIELD_BORDER));
+            rect.setFillColor(Color(144, 238, 144));
+            window.draw(rect);
+        }
         drawTable(&window);
-        drawNums(&window, &font, var_1);
+        drawNums(&window, &regular_font, &bold_font, var_1, table);
         window.display();
-    }
+    }      
     return 0;
 }
